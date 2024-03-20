3016147dfc1e25f59f6a3f9116ddc7d9016dab67
marcos laurenz <75302939+Marcoslaurenz@users.noreply.github.com>
2024-03-20 18:10:22 -0500

    Update droks.py

--- droks.py
+++ droks.py
@@ -1,58 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-proxies = {"http": "http://142.202.220.242:18510",  # Ajusta el esquema del proxy aquí
-        "https": "http://142.202.220.242:18510"} 
-
-
-
+proxies = {"http": "http://142.202.220.242:18510",
+           "https": "http://142.202.220.242:18510"}
 
 
 def verificar_sql_enlace(url):
     try:
-        # Realizar la solicitud GET a la URL
         response = requests.get(url)
-        # Verificar si la solicitud fue exitosa
         if response.status_code == 200:
-            # Analizar el contenido HTML de la respuesta
             soup = BeautifulSoup(response.content, 'html.parser')
-            # Buscar todos los enlaces ('a') en el contenido
             enlaces = soup.find_all('a')
-            # Verificar si alguno de los enlaces contiene ".sql" en su atributo href
             for enlace in enlaces:
                 if enlace.get('href') and '.sql' in enlace.get('href'):
                     print("Valido")
                     print(url)
                     return
-            # Si no se encontró ningún enlace con ".sql", imprimir "invalido"
             print("Invalido")
         else:
-            # Si la solicitud no fue exitosa, imprimir un mensaje de error
             print("Error al realizar la solicitud:", response.status_code)
     except Exception as e:
-        # Manejar cualquier error que ocurra durante el proceso
         print("Error:", e)
 
 
+def search_google(query, api_key, cx, proxies, num_pages):
+    try:
+        for page in range(1, num_pages + 1):
+            start = (page - 1) * 10 + 1  # Calcula el índice de inicio de la página
+            url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={cx}&start={start}"
+            response = requests.get(url, proxies=proxies)
+            if response.status_code == 200:
+                data = response.json()
+                if 'items' in data:
+                    for item in data['items']:
+                        print("Procesando:", item['link'])
+                        verificar_sql_enlace(item['link'])
+                        with open("results.txt", "a+") as r:
+                            r.write(item['link'] + "\n")
+                else:
+                    print("No se encontraron resultados en la página", page)
+            else:
+                print("Error al realizar la búsqueda en la página", page, ":", response.status_code)
+    except Exception as e:
+        print("Error:", e)
 
-
-def search_google(query, api_key, cx,proxies):
-    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={cx}"
-    response = requests.get(url,proxies=proxies)
-    if response.status_code == 200:
-        data = response.json()
-        if 'items' in data:
-            for item in data['items']:
-                #print(item['title'], item['link'])
-                print("Procesando:", item['link'])
-                verificar_sql_enlace(item['link'])
-                with open("results.txt","a+") as r:
-                    r.write(item['link']+"\n")
-        else:
-            print("No se encontraron resultados.")
-    else:
-        print("Error al realizar la búsqueda:", response.status_code)
 
 if __name__ == "__main__":
     print("""
@@ -61,17 +52,13 @@ if __name__ == "__main__":
 ---/|-----------------**---**--**----**-------
 --/-|_-------------****---******---**---------
 -|--|-\------------****---**--**--*****-------
-_|__|_|
+_|__|_|Drophack666
   \_|_/
 """)
-    # Término de búsqueda
     search_query = input('Paste your Query:')
+    num_pages = int(input('Enter the number of pages to extract:'))  # Número de páginas a extraer
 
-    # Clave de API de Google
-    google_api_key = "tu api key de google"
+    google_api_key = "Your Google API Key"
+    google_cx = "Your Google CX"
 
-    # ID de búsqueda de Google (cx)
-    google_cx = "tu id de busqueda de google"
-
-    # Realizar búsqueda en Google
-    search_google(search_query, google_api_key, google_cx,proxies)
+    search_google(search_query, google_api_key, google_cx, proxies, num_pages)
